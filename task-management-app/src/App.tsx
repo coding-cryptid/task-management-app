214d32a9d647c9318570966629900a3de1972d6f
Gianna Cosenza <giacosey28@gmail.com>
2026-07-06 10:24:58 -0400

    Deleted imports of deleted files

--- task-management-app/src/App.tsx
+++ task-management-app/src/App.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 
 
 
