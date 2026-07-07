6f7bb9be7df325802100f434066c0670ef958aba
Gianna Cosenza <giacosey28@gmail.com>
2026-07-07 09:53:05 -0400

    Added new routes for task form and edit form

--- task-management-app/src/App.tsx
+++ task-management-app/src/App.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-import { Auth0Provider } from "@auth0/auth0-react";
-import { useNavigate } from "react-router-dom";
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
 
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TaskList />} />
+        <Route path="/tasks/new" element={<TaskForm />} />
+        <Route path="/tasks/:id/edit" element={<TaskForm />} />
+        <Route path="/tasks/:id" element={<TaskForm />} />
+      </Routes>
+    </Router>
+  );
+};
 
-
-
-export default App
+export default App;
