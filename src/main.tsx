7571203e6f2f3ba5da3cb6279f6a82df9b91dff1
Gianna Cosenza <giacosey28@gmail.com>
2026-07-10 14:17:29 -0400

    Added a space for separation

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { Auth0Provider } from "@auth0/auth0-react";
+
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
