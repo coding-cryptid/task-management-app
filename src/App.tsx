07aee5f7d7f2b62bc869767ee9ccb8102e654ce9
Gianna Cosenza <giacosey28@gmail.com>
2026-07-10 14:40:49 -0400

    Added Auth0 button

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import TaskCreate from './pages/TaskCreate';
 import TaskDetails from './pages/TaskDetails';
 import TaskEdit from './pages/TaskEdit';
 import '/styles.css';
+import AuthButton from './components/AuthButton';
 
 function App() {
   return (
     <TaskProvider>
       <BrowserRouter>
+        <nav className="d-flex justify-content-end p-3 border-bottom">
+          <AuthButton />
+        </nav>
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/tasks/new" element={<TaskCreate />} />
