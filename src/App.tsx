979ee147e93c2ae80484ad823e1facb833e584b9
Gianna Cosenza <giacosey28@gmail.com>
2026-07-11 08:36:02 -0400

    Added routes for login and register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { TaskProvider } from './context/TaskContext';
 import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import TaskCreate from './pages/TaskCreate';
 import TaskDetails from './pages/TaskDetails';
 import TaskEdit from './pages/TaskEdit';
@@ -11,17 +13,17 @@ import '/styles.css';
 function App() {
   return (
     <TaskProvider>
-      <BrowserRouter>
         <nav className="d-flex justify-content-end p-3 border-bottom">
           <AuthButton />
         </nav>
         <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
           <Route path="/tasks/new" element={<ProtectedRoute><TaskCreate /></ProtectedRoute>} />
           <Route path="/tasks/:id" element={<ProtectedRoute><TaskDetails /></ProtectedRoute>} />
           <Route path="/tasks/:id/edit" element={<ProtectedRoute><TaskEdit /></ProtectedRoute>} />
         </Routes>
-      </BrowserRouter>
     </TaskProvider>
   );
 }
