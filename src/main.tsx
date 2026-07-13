7a1b7b62ccacdc597ef80e4befb1cfbabb96d437
Gianna Cosenza <giacosey28@gmail.com>
2026-07-13 09:22:11 -0400

    Added new main routing and imports to handle the new auth0 component

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import { Auth0Provider } from "@auth0/auth0-react";
-import { BrowserRouter } from 'react-router-dom';
-
-const domain = import.meta.env.VITE_AUTH0_DOMAIN;
-const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+import Auth0ProviderWithNavigate from './components/Auth0ProviderWithNavigate'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Auth0Provider
-        domain={domain}
-        clientId={clientId}
-        authorizationParams={{
-          redirect_uri: window.location.origin,
-        }}
-      >
+      <Auth0ProviderWithNavigate>
         <App />
-      </Auth0Provider>
+      </Auth0ProviderWithNavigate>
     </BrowserRouter>
   </StrictMode>,
 )
